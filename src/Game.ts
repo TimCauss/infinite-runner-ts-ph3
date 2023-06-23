0aa424298e18b0efa207e0ca21c791a4461d688b
TimCauss <t.caussignac@gmail.com>
2023-06-23 09:30:30 +0200

    adding auto velocity to the mouse

--- src/Game.ts
+++ src/Game.ts
@@ -8,7 +8,6 @@ export default class Game extends Phaser.Scene {
         super(SceneKeys.Game)
     }
 
-
     create() {
         //store the width and height of the game screen:
         const width = this.scale.width
@@ -17,10 +16,13 @@ export default class Game extends Phaser.Scene {
         //creating the background TileSprite
         this.add.tileSprite(0,0,width,height,'background').setOrigin(0)
 
-        const mouse = this.physics.add.sprite(width * 0.5, height * 0.5, TextureKeys.RocketMouse, 'rocketmouse_fly01.png').play(AnimationKeys.RocketMouseRun)
+        const mouse = this.physics.add.sprite(width * 0.2, height, TextureKeys.RocketMouse, 'rocketmouse_fly01.png').play(AnimationKeys.RocketMouseRun)
 
         const body = mouse.body as Phaser.Physics.Arcade.Body
         body.setCollideWorldBounds(true)
+
+        //Set mouse x velocity:
+        body.setVelocityX(200)
 
         this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30)
     }
