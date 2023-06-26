b32c26cba42f1fedf83d9bda81103dfa98b8ce74
TimCauss <t.caussignac@gmail.com>
2023-06-26 14:59:55 +0200

    feat/fix(RocketMouse/Game): *fixing flames sprites position; *adding cursors class (CursorsKeys); *create a enableJetpack(Boolean) method; *create PreUpdate method; *Adding space bar condition, activate an Y Acceleration and changing the enableJetpack status

--- src/game/RocketMouse.ts
+++ src/game/RocketMouse.ts
@@ -1,11 +1,17 @@
-import Phaser from "phaser";
+import Phaser, { Scene } from "phaser";
 
 import TextureKeys from "../consts/TextureKeys";
 import AnimationKeys from "../consts/AnimationKeys";
 
 export default class RocketMouse extends Phaser.GameObjects.Container {
+  private flames: Phaser.GameObjects.Sprite;
+  private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y);
+
+    //Get CursorKeys Instance
+    this.cursors = scene.input.keyboard.createCursorKeys();
 
     // Create a rocketmouse Sprite:
     const mouse = scene.add
@@ -13,14 +19,16 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
       .setOrigin(0.5, 1)
       .play(AnimationKeys.RocketMouseRun);
 
-    //create the flmes and play the aimation:
-    const flames = scene.add
-      .sprite(0, 0, TextureKeys.RocketMouse)
+    //create the flames and play the aimation:
+    this.flames = scene.add
+      .sprite(-63, -15, TextureKeys.RocketMouse)
       .play(AnimationKeys.RocketFlamesOn);
 
+    this.enableJetpack(false);
+
     // Add the RocketMosue and flames as childs of Container:
+    this.add(this.flames); //Flame first  = behind the mouse
     this.add(mouse);
-    this.add(flames);
 
     // Add a physics body:
     scene.physics.add.existing(this);
@@ -29,5 +37,24 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
     const body = this.body as Phaser.Physics.Arcade.Body;
     body.setSize(mouse.width - 29, mouse.height);
     body.setOffset(mouse.width * -0.5, -mouse.height);
+  }
+
+  preUpdate() {
+    const body = this.body as Phaser.Physics.Arcade.Body;
+
+    //check if Space bar is down:
+    if (this.cursors.space?.isDown) {
+      //set y acceleration to -600
+      body.setAccelerationY(-900);
+      this.enableJetpack(true);
+    } else {
+      //turn off acceleration otherwise:
+      body.setAccelerationY(0);
+      this.enableJetpack(false);
+    }
+  }
+
+  enableJetpack(enabled: boolean) {
+    this.flames.setVisible(enabled);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import Game from './Game'
-import Preloader from './Preloader'
-import SceneKeys from './consts/SceneKeys'
+import Game from "./Game";
+import Preloader from "./Preloader";
+import SceneKeys from "./consts/SceneKeys";
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: 800,
-	height: 640,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-			debug: true,
-		},
-		
-	},
-	scene: [Preloader, Game],
-}
+  type: Phaser.AUTO,
+  parent: "app",
+  width: 800,
+  height: 640,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 600 },
+      debug: true,
+    },
+  },
+  scene: [Preloader, Game],
+};
 
-
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
