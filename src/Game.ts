49c6ebe79ef3c92cde1c70b70d9c3e57d3d96aa2
TimCauss <t.caussignac@gmail.com>
2023-06-26 13:59:50 +0200

    fixig(Game.ts/RocketMouse.ts):hitbox position and size of the mouse

--- src/Game.ts
+++ src/Game.ts
@@ -62,15 +62,8 @@ export default class Game extends Phaser.Scene {
 
     this.bookcases = [this.bookcase1, this.bookcase2];
 
-    const mouse = this.physics.add
-      .sprite(
-        width * 0.5,
-        height - 30,
-        TextureKeys.RocketMouse,
-        "rocketmouse_fly01.png"
-      )
-      .setOrigin(0.5, 1)
-      .play(AnimationKeys.RocketMouseRun);
+    const mouse = new RocketMouse(this, width * 0.5, height - 30)
+    this.add.existing(mouse)
 
     const body = mouse.body as Phaser.Physics.Arcade.Body;
     body.setCollideWorldBounds(true);
--- src/game/RocketMouse.ts
+++ src/game/RocketMouse.ts
@@ -15,5 +15,13 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
 
     // Add as child of Container:
     this.add(mouse);
+
+    // Add a physics body:
+    scene.physics.add.existing(this);
+
+    // adjust physics body size and offset:
+    const body = this.body as Phaser.Physics.Arcade.Body;
+    body.setSize(mouse.width-29, mouse.height);
+    body.setOffset(mouse.width * -0.5, -mouse.height);
   }
 }
